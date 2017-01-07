966e430c21f661a18b5b85dee7951762dbf95686
XetrovTornado <xetrov@icloud.com>
2017-01-06 17:20:16 -1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # StackOverflowParser
-Parses a StackOverflow page to show posts in a terminal. Goal is to search StackOverflow for answers in the terminal.
+Parses a StackOverflow page to show posts in a terminal. 
+Goal is to search StackOverflow for answers in the terminal.
+
+### How to use
+1. Run soparser.py in python3 and add a StackOverflow page as an extra argument
+2. Posts are seperated by lines of dashes
+3. Links are in parentheses
+4. When prompted for the next post, type n or no and press RETURN to quit or press RETURN without typing anything to continue
+
+### Goals
+- Auto search StackOverflow and StackExchange for an answer
+- Provide symbols for quotes and code
---
+++ soparser.py
@@ -0,0 +1,51 @@
+import urllib.request as u
+from html.parser import HTMLParser
+import sys
+
+# Get a website from either the command line or a sample web page
+try:
+	website = sys.argv[1]
+except IndexError:
+	print("No webpage was provided.")
+	sys.exit()
+
+if not "stackoverflow.com" in website and not "stackexchange.com" in website:
+	print("Only StackOverflow webpages are supported.")
+	sys.exit()
+
+# Extract the html from the webpage
+html = u.urlopen(website).read().decode('utf-8')
+
+# Parse the data
+class WebScraper(HTMLParser):
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.startfound = False
+		self.divsfound = 0
+	def handle_starttag(self, tag, attrs):
+		# print("found starttag, tag=" + tag + " attrs=" + str(attrs))
+		if tag == "div" and len(attrs) > 0 and ("class", "post-text") in attrs:
+			#print("start found")
+			self.startfound = True
+		if tag == "div" and self.startfound:
+			#print("div found: " + str(self.divsfound))
+			self.divsfound += 1
+		if tag == "a" and self.startfound:
+			print("(%s)" % attrs[0][1])
+	def handle_endtag(self, tag):
+		if tag == "div" and self.startfound:
+			#print("end div found")
+			self.divsfound -= 1
+			if self.divsfound <= 0:
+				print("\n------------------")
+				continu = input("Next post? ")
+				if continu.startswith("n"):
+					sys.exit()
+				self.startfound = False
+	def handle_data(self, data):
+		if not data.strip() == "" and self.startfound:
+			print(data, end="")
+		pass
+
+WebScraper().feed(html)
+
